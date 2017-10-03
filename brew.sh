6646056c951e4764f073efe037a2e768a7ab2df0
Andrew Podkovyrin <podkovyrin@gmail.com>
2017-10-03 19:15:33 +0300

    Use system bash; Add 'gemu' alias

--- brew.sh
+++ brew.sh
@@ -8,17 +8,7 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade
 
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
 brew install bash-completion
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
