7873c81449e0dca15650d24e8e699be3b152b339
Andrew Podkovyrin <podkovyrin@gmail.com>
2019-06-23 15:54:08 +0300

    Fix $PATH

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,14 +1,18 @@
 # path, the 0 in the filename causes this to load first
 
-pathAppend() {
+pathInsert() {
   # Only adds to the path if it's not already there
   if ! echo $PATH | egrep -q "(^|:)$1($|:)" ; then
-    PATH=$PATH:$1
+    PATH=$1:$PATH
   fi
 }
 
 # Remove duplicate entries from PATH:
 PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
 
-pathAppend "$HOME/.yadr/bin"
-pathAppend "$HOME/.yadr/bin/yadr"
+pathInsert "$HOME/.yadr/bin"
+pathInsert "$HOME/.yadr/bin/yadr"
+
+pathInsert "$HOME/.fastlane/bin"
+pathInsert "/usr/local/sbin"
+pathInsert "/usr/local/opt/ruby/bin"
