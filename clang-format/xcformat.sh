48a514e90a6b3c707b2b3d47d80f6b7999cbd197
Andrew Podkovyrin <podkovyrin@gmail.com>
2019-07-10 21:50:45 +0300

    Use default clang-format binary; Fix clang-format config

--- clang-format/xcformat.sh
+++ clang-format/xcformat.sh
@@ -18,4 +18,4 @@ end tell ')
 LOGNAME=~/.lastclangformatlog.txt
 echo "Filepath: ${CURRENT_DOCUMENT_PATH}" > ${LOGNAME}
 
-~/.yadr/clang-format/clang-format-custom -i -style=file "${CURRENT_DOCUMENT_PATH}" >> ${LOGNAME} 2>&1
+/usr/local/bin/clang-format -i -style=file "${CURRENT_DOCUMENT_PATH}" >> ${LOGNAME} 2>&1
