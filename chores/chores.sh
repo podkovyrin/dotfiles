592b4e90eacba2d22e50d11d53533629c94bd0c5
Andrew Podkovyrin <podkovyrin@gmail.com>
2020-06-01 21:53:08 +0300

    add 'mas upgrade' to chores

--- chores/chores.sh
+++ chores/chores.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-echo "🧹 Running chores (brew & gem update, cleanup)"
+echo "🧹 Running chores (brew, gem and AppStore updates, cleanup)"
 
 sudo -v
+# Keep-alive: update existing `sudo` time stamp until `chores` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 echo "🧹 brew"
 brew update && brew upgrade && brew cleanup && brew doctor
 
 echo "🧹 gem"
 gem update && gem cleanup
+
+echo "🧹 AppStore"
+mas upgrade
 
 echo "🧹 cleanup"
 CMM=$HOME/.yadr/clean_my_mac/clean_my_mac.sh
