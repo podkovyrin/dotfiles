2550f26f9e4195e8c50fad3d78f87433130fb0ce
Andrew Podkovyrin <podkovyrin@gmail.com>
2021-12-26 23:37:26 +0100

    update

--- chores/chores.sh
+++ chores/chores.sh
@@ -9,14 +9,8 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 echo "🧹 brew"
 brew update && brew upgrade && brew cleanup && brew doctor
 
-echo "🧹 cask"
-brew cask upgrade --greedy && brew cask doctor
-
 echo "🧹 gem"
 gem update && gem cleanup
-
-echo "🧹 AppStore"
-mas upgrade
 
 echo "🧹 cleanup"
 CMM=$HOME/.yadr/clean_my_mac/clean_my_mac.sh
