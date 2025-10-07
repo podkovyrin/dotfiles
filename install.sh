75978bddcc69e663a6c2bdec9863ba408cc17ee9
Andrei Podkovyrin <podkovyrin@gmail.com>
2025-10-07 09:52:35 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -2,6 +2,19 @@
 
 if [ ! -d "$HOME/.dotfiles" ]; then
     echo "Installing dotfiles for the first time"
+
+    if ! xcode-select -p &>/dev/null; then
+        echo "Installing Xcode Command Line Tools..."
+        xcode-select --install
+        # wait until the Xcode Command Line Tools are installed
+        until xcode-select -p &>/dev/null; do
+            sleep 5
+        done
+        echo "Xcode Command Line Tools installed."
+    else
+        echo "Xcode Command Line Tools already installed."
+    fi
+
     git clone https://github.com/podkovyrin/dotfiles.git "$HOME/.dotfiles"
     cd "$HOME/.dotfiles"
 
